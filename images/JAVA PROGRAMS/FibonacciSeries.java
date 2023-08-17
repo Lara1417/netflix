3481673a4acfaa5fa4cee0a6c50333bee1ebc23d
Lara1417 <101194704+Lara1417@users.noreply.github.com>
2023-08-17 15:06:36 +0530

    Add files via upload

---
+++ images/JAVA PROGRAMS/FibonacciSeries.java
@@ -0,0 +1,18 @@
+class  FibonacciSeries
+{
+	public static void main(String[] args) 
+	{
+		int a=0;
+		int b=1;
+		int count=10;
+
+		System.out.println(a+""+b);
+		for(i=1;i<10;i++)
+		{
+			c=a+b;
+			System.out.println(""+c)
+				a=b;
+			    b=c;
+		}
+	}
+}
